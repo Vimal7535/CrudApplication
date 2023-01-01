9227a128a3a89628291271c05d63a210bbd75b5e
chhavi raj singh <chhavirajsingh2000@gmail.com>
2023-01-01 21:42:19 +0530

    Add project files.

---
+++ CrudApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,74 @@
+﻿using CrudApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var res = context.Employees.ToList();
+            return View(res);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var emp = new Employee()
+                {
+                    Name = obj.Name,
+                    City = obj.City,
+                    State = obj.State,
+                    Salary = obj.Salary,
+
+                };
+                context.Employees.Add(obj);
+                context.SaveChanges();
+                TempData["Error"] = " Record Save";
+                return RedirectToAction("index");
+            }
+            else
+            {
+                TempData["error"] = "empty field con't submit";
+                return View(obj);
+            }
+            
+        }
+        public IActionResult Edit(int? id)
+        {
+            var Et = context.Employees.Where(x => x.Id == id).FirstOrDefault();
+            return View(Et);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee a)
+        {
+            context.Entry(a).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            TempData["Error"] = "Record updated";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var Dt = context.Employees.Where(x => x.Id == id).FirstOrDefault();
+            context.Employees.Remove(Dt);
+            context.SaveChanges();
+            TempData["Error"] = "Record Deleted";
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ CrudApplication/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApplication.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="* Name can't blank")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "City can't blank")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "State can't blank")]
+        public string State { get; set; }
+        public int Salary { get; set; }
+    }
+}
